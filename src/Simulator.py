bfdbbc3aefe01daeae6c71c0f453bb99a715e252
Jon Tedesco <jon.c.tedesco@gmail.com>
2012-04-13 00:06:04 -0500

    Removed server side exception handling

--- src/Simulator.py
+++ src/Simulator.py
@@ -30,35 +30,29 @@ class Simulator(object):
             @return the client id
         """
 
-        try:
-
-            # Get this client's id
-            global nextClientId
-            clientId = nextClientId
-            nextClientId += 1
+        # Get this client's id
+        global nextClientId
+        clientId = nextClientId
+        nextClientId += 1
 
 
-            global logMessagesLock
-            logMessagesLock.acquire()
+        global logMessagesLock
+        logMessagesLock.acquire()
 
-            logMessagesLock[clientId] = {
-                'updates' : Queue(),
-                'clientId'  : clientId,
-                'trigger'   : threading.Semaphore()
-            }
-            logMessagesLock.release()
+        logMessagesLock[clientId] = {
+            'updates' : Queue(),
+            'clientId'  : clientId,
+            'trigger'   : threading.Semaphore()
+        }
+        logMessagesLock.release()
 
 
-            # Respond to the client's id
-            cherrypy.response.headers['Content-Type'] = 'application/json'
-            return dumps({
-                'id': clientId,
-                'successful': True
-            })
-
-        except Exception, error:
-            logMessagesLock.release()
-            raise HTTPError(message=error.message)
+        # Respond to the client's id
+        cherrypy.response.headers['Content-Type'] = 'application/json'
+        return dumps({
+            'id': clientId,
+            'successful': True
+        })
 
 
     @cherrypy.expose
@@ -67,23 +61,17 @@ class Simulator(object):
           Removes a client from the log stream
         """
 
-        try:
+        global logMessagesLock
+        logMessagesLock.acquire()
 
-            global logMessagesLock
-            logMessagesLock.acquire()
+        del logMessagesLock[clientId]
+        logMessagesLock.release()
 
-            del logMessagesLock[clientId]
-            logMessagesLock.release()
-
-            # Send back the status of the unsubscribe request
-            cherrypy.response.headers['Content-Type'] = 'application/json'
-            return dumps({
-                'successful': True
-            })
-
-        except Exception, error:
-            logMessagesLock.release()
-            raise HTTPError(message=error.message)
+        # Send back the status of the unsubscribe request
+        cherrypy.response.headers['Content-Type'] = 'application/json'
+        return dumps({
+            'successful': True
+        })
 
 
     @cherrypy.expose
