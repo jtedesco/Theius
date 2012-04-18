7818577af149e954724ee56373258e06e308abfd
Jon Tedesco <jon.c.tedesco@gmail.com>
2012-04-18 16:56:48 -0500

    Test commit

--- src/SimulatorThread.py
+++ src/SimulatorThread.py
@@ -5,8 +5,6 @@ from time import sleep
 __author__ = 'jon'
 
 class SimulatorThread(threading.Thread):
-
-
     def __init__(self, logMessages, serverLock, keys):
         """
           Initialize the simulator thread, given the <code>logMessages</code> map and a lock to access it
@@ -23,7 +21,7 @@ class SimulatorThread(threading.Thread):
         self.colors = ["red", "yellow", "green"]
 
         # possible value for each node
-        self.values = range(1,10)
+        self.values = range(1, 10)
 
     def run(self):
         """
@@ -31,9 +29,8 @@ class SimulatorThread(threading.Thread):
         """
 
         while True:
-
             # Insert some random delay between 0 and 2 second
-            sleep(2*random())
+            sleep(2 * random())
 
             # Get the new log event
             logEvent = {
@@ -50,7 +47,6 @@ class SimulatorThread(threading.Thread):
             self.serverLock.acquire()
 
             for clientId in self.logMessages:
-
                 # Add the new log event to the client's queue
                 self.logMessages[clientId]['updates'].put(logEvent)
 
@@ -67,4 +63,4 @@ class SimulatorThread(threading.Thread):
         index = int(random() * len(array))
         if index == len(array):
             index -= 1
-        return array[index];
+        return array[index]
