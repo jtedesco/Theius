69976fb6e304a0480d6d93850fe5c2e5fa2ddad1
Jon Tedesco <jon.c.tedesco@gmail.com>
2012-04-19 15:27:32 -0500

    Implemented multi-key lookup for Splom

--- static/scripts/visualizations/SplomVisualization.js
+++ static/scripts/visualizations/SplomVisualization.js
@@ -4,19 +4,133 @@
 function SplomVisualization(structure, state) {
 
 
+    // Get the racks to build the categories list of points
+    var racks = [];
+    var nodesByRack = {};
+    if(structure.hasOwnProperty('children')) {
+        for(var i in structure['children']) {
+            if (structure['children'].hasOwnProperty(i)) {
+                var rack = structure['children'][i];
+                var rackName = rack['name'];
 
+                if($.inArray(rackName, racks) < 0) {
+                    racks.push(rackName);
+                    nodesByRack[rackName] = [];
+                }
+
+                var rack2 = rack['children'];
+                for (var machineName in rack2) {
+                    if (rack['children'].hasOwnProperty(machineName)) {
+                        rack['children'][machineName]['rack'] = rackName;
+                        nodesByRack[rackName].push(rack['children'][machineName]);
+                    }
+                }
+            }
+        }
+    } else {
+        racks.push('default');
+    }
+
+    // Build the traits to use for visualizing
+    var traits = [
+        'predictedSeverityProbabilities.INFO',
+        'predictedSeverityProbabilities.WARN',
+        'predictedSeverityProbabilities.ERROR',
+        'predictedSeverityProbabilities.FATAL'
+    ];
+
+    // Form the data
+    var values = [];
+    for (var name in state) {
+        if (state.hasOwnProperty(name) && name.indexOf('machine') === 0) {
+            values.push(state[name]); // Get a handle on the state of this machine, which will update
+        }
+    }
+
+    var data = {
+        traits: traits,
+        racks: racks,
+        values: values
+    };
+
+    var plot = function(p) {
+        var cell = d3.select(this);
+
+        // Plot frame.
+        cell.append("rect")
+            .attr("class", "frame")
+            .attr("x", padding / 2)
+            .attr("y", padding / 2)
+            .attr("width", size - padding)
+            .attr("height", size - padding);
+
+        // Plot dots.
+        cell.selectAll("circle")
+            .data(data.values)
+            .enter().append("circle")
+            .attr("class", function(machine) { return machine.rackName; })
+            .attr("cx", function(d) { return x[p.x](d[p.x]); })
+            .attr("cy", function(d) { return y[p.y](d[p.y]); })
+            .attr("r", 3);
+
+        // Plot brush.
+        cell.call(brush.x(x[p.x]).y(y[p.y]));
+    };
+
+    // Clear the previously-active brush, if any.
+    var brushstart = function(p) {
+        if (brush.data !== p) {
+            cell.call(brush.clear());
+            brush.x(x[p.x]).y(y[p.y]).data = p;
+        }
+    };
+
+    // Highlight the selected circles.
+    var brush = function(p) {
+        var e = brush.extent();
+        svg.selectAll("circle").attr("class", function(machine) {
+            return e[0][0] <= machine[p.x] && machine[p.x] <= e[1][0]
+                && e[0][1] <= machine[p.y] && machine[p.y] <= e[1][1]
+                ? machine.rackName : null;
+        });
+    };
+
+    // If the brush is empty, select all circles.
+    var brushend = function() {
+        if (brush.empty()) svg.selectAll("circle").attr("class", function(machine) {
+            return machine.rackName;
+        });
+    };
+
+    var cross = function(a, b) {
+        var c = [], n = a.length, m = b.length, i, j;
+        for (i = -1; ++i < n;) for (j = -1; ++j < m;) c.push({x: a[i], i: i, y: b[j], j: j});
+        return c;
+    };
 
 
     // Size parameters.
     var size = 150,
         padding = 19.5,
-        n = flower.traits.length;
+        n = data.traits.length;
 
     // Position scales.
     var x = {}, y = {};
-    flower.traits.forEach(function(trait) {
-        var value = function(d) { return d[trait]; },
-            domain = [d3.min(flower.values, value), d3.max(flower.values, value)],
+    data.traits.forEach(function(trait) {
+        var value = function(machineData) {
+
+            // Split the trait by period if necessary, but only once (allows for keys of dictionaries)
+            var keys = trait.split('.');
+            if (keys.length > 2) {
+                console.log('Cannot use more than one level of nested keys!');
+                return null;
+            } else if (keys.length === 2) {
+                return machineData[keys[0]][keys[1]];
+            } else {
+                return machineData[keys[0]];
+            }
+        },
+            domain = [d3.min(data.values, value), d3.max(data.values, value)],
             range = [padding / 2, size - padding / 2];
         x[trait] = d3.scale.linear()
             .domain(domain)
@@ -39,13 +153,13 @@ function SplomVisualization(structure, state) {
         .on("brushend", brushend);
 
     // Root panel.
-    var svg = d3.select("#chart").append("svg")
+    var svg = d3.select(".visualization").append("svg")
         .attr("width", size * n + padding)
         .attr("height", size * n + padding);
 
     // X-axis.
     svg.selectAll("g.x.axis")
-        .data(flower.traits)
+        .data(data.traits)
         .enter().append("g")
         .attr("class", "x axis")
         .attr("transform", function(d, i) { return "translate(" + i * size + ",0)"; })
@@ -53,7 +167,7 @@ function SplomVisualization(structure, state) {
 
     // Y-axis.
     svg.selectAll("g.y.axis")
-        .data(flower.traits)
+        .data(data.traits)
         .enter().append("g")
         .attr("class", "y axis")
         .attr("transform", function(d, i) { return "translate(0," + i * size + ")"; })
@@ -61,7 +175,7 @@ function SplomVisualization(structure, state) {
 
     // Cell and plot.
     var cell = svg.selectAll("g.cell")
-        .data(cross(flower.traits, flower.traits))
+        .data(cross(data.traits, data.traits))
         .enter().append("g")
         .attr("class", "cell")
         .attr("transform", function(d) { return "translate(" + d.i * size + "," + d.j * size + ")"; })
@@ -73,59 +187,4 @@ function SplomVisualization(structure, state) {
         .attr("y", padding)
         .attr("dy", ".71em")
         .text(function(d) { return d.x; });
-
-    var plot = function(p) {
-        var cell = d3.select(this);
-
-        // Plot frame.
-        cell.append("rect")
-            .attr("class", "frame")
-            .attr("x", padding / 2)
-            .attr("y", padding / 2)
-            .attr("width", size - padding)
-            .attr("height", size - padding);
-
-        // Plot dots.
-        cell.selectAll("circle")
-            .data(flower.values)
-            .enter().append("circle")
-            .attr("class", function(d) { return d.species; })
-            .attr("cx", function(d) { return x[p.x](d[p.x]); })
-            .attr("cy", function(d) { return y[p.y](d[p.y]); })
-            .attr("r", 3);
-
-        // Plot brush.
-        cell.call(brush.x(x[p.x]).y(y[p.y]));
-    }
-
-    // Clear the previously-active brush, if any.
-    var brushstart = function(p) {
-        if (brush.data !== p) {
-            cell.call(brush.clear());
-            brush.x(x[p.x]).y(y[p.y]).data = p;
-        }
-    };
-
-    // Highlight the selected circles.
-    var brush = function(p) {
-        var e = brush.extent();
-        svg.selectAll("circle").attr("class", function(d) {
-            return e[0][0] <= d[p.x] && d[p.x] <= e[1][0]
-                && e[0][1] <= d[p.y] && d[p.y] <= e[1][1]
-                ? d.species : null;
-        });
-    };
-
-    // If the brush is empty, select all circles.
-    var brushend = function() {
-        if (brush.empty()) svg.selectAll("circle").attr("class", function(d) {
-            return d.species;
-        });
-    };
-
-    var cross = function(a, b) {
-        var c = [], n = a.length, m = b.length, i, j;
-        for (i = -1; ++i < n;) for (j = -1; ++j < m;) c.push({x: a[i], i: i, y: b[j], j: j});
-        return c;
-    };
 }
